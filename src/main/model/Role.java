e6838300ab79d85242e932c3082ff02af047be25
Traveler1357 <777kovalik@gmail.com>
2019-06-24 21:11:12 +0300

    Added User and Role entities

---
+++ src/main/model/Role.java
@@ -0,0 +1,45 @@
+package main.model;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "roles")
+public class Role {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column
+    private String name;
+
+    @Column
+    private String description;
+
+    public Long getId() {
+        return id;
+    }
+
+    public Role setId(Long id) {
+        this.id = id;
+        return this;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Role setName(String name) {
+        this.name = name;
+        return this;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public Role setDescription(String description) {
+        this.description = description;
+        return this;
+    }
+}
