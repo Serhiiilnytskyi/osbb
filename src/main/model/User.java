5c9ed3bd07d34d111837b0a36b5f03c6798a9341
Traveler1357 <777kovalik@gmail.com>
2019-06-24 21:11:12 +0300

    Added User and Role entities

---
+++ src/main/model/User.java
@@ -0,0 +1,68 @@
+package main.model;
+
+import javax.persistence.*;
+import java.util.Set;
+
+    @Entity
+    @Table(name = "user")
+    public class User {
+
+        @Id
+        @GeneratedValue(strategy = GenerationType.IDENTITY)
+        @Column(name = "id", unique = true, nullable = false)
+        private Long id;
+
+        @Column(name = "first_name", unique = true, nullable = false)
+        private String firstName;
+
+        @Column(name = "second_name", unique = true, nullable = false)
+        private String secondName;
+
+        @Column(name = "email", unique = true)
+        private String email;
+
+        @Column(name = "password")
+        private String password;
+
+        @ManyToMany(cascade=CascadeType.ALL,fetch=FetchType.EAGER)
+        @JoinTable(name="user_roles",
+                joinColumns = {@JoinColumn(name="user_id", referencedColumnName="id")},
+                inverseJoinColumns = {@JoinColumn(name="role_id", referencedColumnName="id")})
+        private Set<Role> roles;
+
+        public Long getId() {
+            return id;
+        }
+
+        public User setId(Long id) {
+            this.id = id;
+            return this;
+        }
+
+        public String getEmail() {
+            return email;
+        }
+
+        public User setEmail(String email) {
+            this.email = email;
+            return this;
+        }
+
+        public String getPassword() {
+            return password;
+        }
+
+        public  User setPassword(String password) {
+            this.password = password;
+            return this;
+        }
+
+        public Set<Role> getRoles() {
+            return roles;
+        }
+
+        public User setRoles(Set<Role> roles) {
+            this.roles = roles;
+            return this;
+        }
+}
