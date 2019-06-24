dac3e5a499612de943111bfab1884d0fdc6e193b
Traveler1357 <777kovalik@gmail.com>
2019-06-24 21:45:50 +0300

    merge commit

--- src/main/java/com/lits/osbb/model/User.java
+++ src/main/java/com/lits/osbb/model/User.java
@@ -7,6 +7,7 @@ import java.util.Set;
     @Table(name = "user")
     public class User {
 
+
         @Id
         @GeneratedValue(strategy = GenerationType.IDENTITY)
         @Column(name = "id", unique = true, nullable = false)
