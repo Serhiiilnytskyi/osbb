230ff934bec71f714e4246a82a09cfc73ec5a2e5
Voronovskyy <48327247+Voronovskyy@users.noreply.github.com>
2019-07-24 22:43:47 +0300

    Created VotingPostController, updated VotingPostSerivce, VotingPostServiceImpl, VotingPostRepository (#15)
    * Code fixes (#11)
    
    * Changed field autorwires to constructor autowires
    
    * Added initialization for all ArrayLists and HashSets
    
    * Fixed liqubase
    
    * Added OSBB entity
    
    * Added Osbb entity. Fix liqubase. Added relationship osbb-user
    
    * Changed types Date to LocalDateTime
    
    * Fix pom.xml, added packaging and swagger version property
    
    * Created VotingPostController, updated VotingPostSerivce, VotingPostServiceImpl, VotingPostRepository
    
    * Created VotingPostController, updated VotingPostSerivce, VotingPostServiceImpl, VotingPostRepository

--- src/main/java/com/lits/osbb/repository/VotingPostRepository.java
+++ src/main/java/com/lits/osbb/repository/VotingPostRepository.java
@@ -4,15 +4,23 @@ import com.lits.osbb.model.VotingPost;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.Optional;
+import java.util.List;
 
 @Repository
 public interface VotingPostRepository extends CrudRepository<VotingPost, Long> {
 
-    Optional<VotingPost> findById(Long id);
+    VotingPost findOne(Long id);
 
-    VotingPost findByTitleContains(String title);
+    VotingPost findOneByTitle(String title);
 
-    VotingPost findByAuthor(String title);
+    VotingPost findOneByAuthor(String author);
+
+    List<VotingPost> findAll ();
+
+    VotingPost update(VotingPost votingPost);
+
+    VotingPost save(VotingPost votingPost);
+
+    void delete(VotingPost votingPost);
 
 }
